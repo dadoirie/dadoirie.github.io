ef2950f33314a829f823b890c0bf058c033bbe4d
damir.krupic@gmail.com <damir.krupic@gmail.com>
2021-08-31 10:56:18 +0200

    reset

--- js/main.js
+++ js/main.js
@@ -13,12 +13,13 @@ function toggleLightDark() {
   }
 }
 
+/*
 fetch("calculator.html")
   .then(response => {
     return response.text()
   })
-  .then(data => {
-    document.getElementById('content').innerHTML = data;
+  .then(calculator => {
+    document.getElementById('content').innerHTML = calculator;
 //    document.getElementById('scriptID').setAttribute('src', "js/calculator.js");
     var s = document.createElement('script');
     s.setAttribute('src', "js/calculator.js");
@@ -26,21 +27,13 @@ fetch("calculator.html")
     document.body.appendChild(s);
   });
   
-  function removejscssfile(filename, filetype) {
-    var targetelement = (filetype == "js") ? "script" : (filetype == "css") ? "link" : "none" //determine element type to create nodelist from
-    var targetattr = (filetype == "js") ? "src" : (filetype == "css") ? "href" : "none" //determine corresponding attribute to test for
-    var allsuspects = document.getElementsByTagName(targetelement)
-    for (var i = allsuspects.length; i >= 0; i--) { //search backwards within nodelist for matching elements to remove
-      if (allsuspects[i] && allsuspects[i].getAttribute(targetattr) != null && allsuspects[i].getAttribute(targetattr).indexOf(filename) != -1)
-        allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
-    }
-  }
   /*
   var s = document.createElement('script');
   s.setAttribute('src', "js/calculator.js");
   s.setAttribute('id', "scriptID");
   document.body.appendChild(s);
-  */
+  
   (function() {
-    removejscssfile("js/calculator.js", "js")
+    // body
   })();
+  */
